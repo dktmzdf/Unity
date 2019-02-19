47e877deb29ae986a6057ee1d597b8dd18c77259
dktmzdf <p93006@hanmail.net>
2019-02-19 14:58:11 +0900

    first commit

---
+++ Particle_shader/Assets/Scripts/JoystickManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickManager : MonoBehaviour
+{
+    public Transform circle;
+    public Transform backCircle;
+    public Vector3 PointA;
+    public Vector3 PointB;
+    Vector2 distance;
+    public bool isJoystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        isJoystick = true;
+        circle.gameObject.SetActive(false);
+        backCircle.gameObject.SetActive(false);
+        circle.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        backCircle.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.mousePosition.x <= (Screen.width / 5) * 2)
+            {
+                circle.gameObject.SetActive(true);
+                backCircle.gameObject.SetActive(true);
+                PointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+                circle.transform.position = PointA - new Vector3(0, 0, PointA.z);
+                backCircle.transform.position = PointA - new Vector3(0, 0, PointA.z);
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            PointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            circle.gameObject.SetActive(false);
+            backCircle.gameObject.SetActive(false);
+            PointB = PointA;
+            circle.transform.position = backCircle.transform.position;
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        if(isJoystick)
+        {
+            Vector2 offset = PointB - PointA;
+            
+            distance = Vector2.ClampMagnitude(offset,1f);
+            circle.transform.position = backCircle.transform.position + new Vector3(distance.x, distance.y, 0);
+        }
+    }
+}
---
+++ Particle_shader/Assets/Scripts/MapManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    //public Texture BackGround;
+    public Texture2D myTexture;
+    public Material MyMaterial;
+    public GameObject myBack;
+    public float speed;
+
+    
+    public GameObject[] prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Color32[] col = myTexture.GetPixels32();
+        
+        for (int i = 0; i < myTexture.height; i++)
+        { 
+            for (int j = 0; j < myTexture.width; j++)
+            {
+
+
+                //Debug.Log();
+                for (int k = 0; k < prefabs.Length; k++)
+                {
+                    
+                    if (col[myTexture.height * i + j] ==
+                        prefabs[k].transform.GetChild(0).GetComponent<MeshRenderer>().material.color)
+                        Instantiate(prefabs[k], new Vector3(myTexture.height - i, myTexture.width - j, 0), Quaternion.identity);
+                }
+                
+                    
+            }
+        }
+        speed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+    }
+    /*
+    void MoveTexture()
+    {
+        float offset = speed * Time.deltaTime;
+        myBack.GetComponent<Renderer>().material.mainTexture = BackGround;
+        myBack.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(offset,0);
+        //myBack.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+
+        //float scaleX = Mathf.Cos(Time.time) * 0.5f * speed;
+        //float scaleY = Mathf.Sin(Time.time) * 0.5f * speed;
+    }*/
+}
